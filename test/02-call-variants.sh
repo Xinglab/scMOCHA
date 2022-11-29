ba0507c03503f0d3d86c6bddec1b71ff98ff4eff
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-11-29 13:17:13 -0500

    call variants with cellsnp-lite

---
+++ test/02-call-variants.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2022-11-29 11:34:42
+# @DESCRIPTION:
+
+# Number of input parameters
+param=$#
+
+/home/liuc9/tools/cellsnp-lite-1.2.2/bin/bin/cellsnp-lite \
+  -s /home/liuc9/github/scRNAseq-MitoVariant/data/pbmc_10k_v3_possorted_genome_bam.bam \
+  -b /home/liuc9/github/scRNAseq-MitoVariant/data/pbmc_10k_v3_filtered_feature_bc_matrix/barcodes.tsv.gz \
+  -O cellsnp \
+  --chrom=MT \
+  --UMItag Auto \
+  --minMAF 0 \
+  --minCOUNT 0 \
+  --genotype \
+  --gzip \
+  -p 10
