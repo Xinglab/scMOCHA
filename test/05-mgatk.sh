a9634578dd34fdcb2d991379724e882c900f5b1d
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-01-23 22:54:00 -0500

    mgatk

---
+++ test/05-mgatk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2023-01-23 16:05:43
+# @DESCRIPTION:
+
+# Number of input parameters
+param=$#
+
+~/tools/anaconda3/bin/mgatk tenx -i /scr1/users/liuc9/mitochondrial/PBMC_10k_v3_10x/pbmc_10k_v3_possorted_genome_bam.bam \
+  -n CRR_test1 -o CRR_test1_mgatk -c 12 -ub UB \
+  -bt CB -b /scr1/users/liuc9/mitochondrial/PBMC_10k_v3_10x/pbmc_10k_v3_filtered_feature_bc_matrix/barcodes.tsv.gz \
+  --mito-genome /home/liuc9/scratch/mitochondrial/rCRS/NC_012920.rCRS.fasta
+
+~/tools/anaconda3/bin/mgatk tenx -i /scr1/users/liuc9/tools/mgatk/tests/barcode/test_barcode.bam \
+  -n CRR_test1 -o CRR_test1_mgatk -c 12 -ub UB \
+  -bt CB -b /scr1/users/liuc9/tools/mgatk/tests/barcode/test_barcodes.txt
