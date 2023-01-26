83033cb0943e743d866852b50575cee27b47a078
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-01-26 13:32:41 -0500

    mgatk

--- test/05-mgatk.R
+++ test/05-mgatk.R
@@ -71,17 +71,118 @@ hetero %>%
   labs(
     x = "Cell",
     y = "MT variant"
-  )
+  ) ->
+  tileplot
 
+ggsave(
+  filename = "mgatk_cell_genotype_tileplot.pdf",
+  plot = tileplot,
+  device = "pdf",
+  path = "data/PBMC_10k_v3_10x/result/04-allele-freq/",
+  width = 12,
+  height = 9
+)
+
+depthtable <- readr::read_tsv(file = "/home/liuc9/scratch/mitochondrial/PBMC_10k_v3_10x/MTbam/mgatk/final/mgatk.depthTable.txt", col_names = c("barcode", "depth"))
+
+depthtable %>% 
+  ggplot(aes(x = depth)) +
+  geom_density()
+
+depthtable$depth %>% quantile()
+
+
+coverage <- vroom::vroom(file = "/scr1/users/liuc9/mitochondrial/PBMC_10k_v3_10x/MTbam/mgatk/final/mgatk.coverage.txt.gz", delim = ",", col_names = c("pos", "barcode", "depth"))
+
+coverage %>% 
+  tidyr::pivot_wider(
+    names_from = pos,
+    values_from = depth
+  ) ->
+  coverage_wider
 
 # seurat ------------------------------------------------------------------
 
 
 
-sct_cluster <- readr::read_rds()
+sct_cluster <- readr::read_rds("data/PBMC_10k_v3_10x/rda/pbmc_sct_cluster_annotated.rds.gz")
+
+
+umap <- sct_cluster@reductions$umap@cell.embeddings
+colnames(umap) <- c("UMAP_1", "UMAP_2")
+
+cluster <- sct_cluster@meta.data[, c("seurat_clusters", "sctype")] %>% 
+  dplyr::rename(cluster = seurat_clusters, celltype =  sctype)
+
+hetero %>% 
+  tidyr::pivot_wider(names_from  = variant, values_from = af) ->
+  hetero_w
+
+dplyr::bind_cols(umap, cluster) %>% 
+  tibble::rownames_to_column(var = "barcode") %>% 
+  dplyr::left_join(hetero_w, by = "barcode") %>% 
+  dplyr::mutate(
+    dplyr::across(
+    dplyr::everything(), 
+    ~tidyr::replace_na(.x, 0)
+    )) ->
+  cell_cluster_af
+
+cell_cluster_af %>% 
+  dplyr::left_join(depthtable, by = "barcode") ->
+  cell_cluster_af_depth
+
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
+myPalette <- colorRampPalette(c("#969696", "#fa0202"))
+sc <- scale_colour_gradientn(colours = myPalette(100), limits=c(0, 1))
+
+cell_cluster_af %>% 
+  # dplyr::filter(variant == "2617A>G") %>% nrow()
+  ggplot() +
+  geom_point(
+    aes(
+      x = UMAP_1,
+      y = UMAP_2,
+      colour = `9966G>A`,
+      shape = NULL,
+      alpha = NULL
+    ),
+    size = 0.7
+  ) +
+  sc
+
+
+cell_cluster_af_depth %>% 
+  ggplot(aes(
+    x = depth,
+    y = `9966G>A`,
+  )) +
+  geom_point()
+
+
+cell_cluster_af_depth %>% 
+  dplyr::select(barcode, depth, `9966G>A`) %>% 
+  dplyr::inner_join(
+    coverage_wider %>% 
+      dplyr::select(barcode, `9966`),
+    by = "barcode"
+  ) %>% 
+  dplyr::mutate(
+    dplyr::across(
+      dplyr::everything(), 
+      ~tidyr::replace_na(.x, 0)
+    )) %>% 
+  ggplot(aes(
+    x = `9966`,
+    y = `9966G>A`,
+  )) +
+  geom_point(position = position_jitter())
 
 # footer ------------------------------------------------------------------
 
 # future::plan(future::sequential)
 
 # save image --------------------------------------------------------------
+save.image(
+  file = "data/PBMC_10k_v3_10x/rda/05-mgatk.rda"
+)
