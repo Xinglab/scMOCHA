2da5bf09bc4864e98b39551e347d278c98244fcc
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-02-22 18:25:48 -0500

    run cell ranger

--- test/00-run-cellranger.sh
+++ test/00-run-cellranger.sh
@@ -9,7 +9,7 @@ param=$#
 
 /home/liuc9/tools/cellranger-7.0.1/bin/cellranger count \
   --id=pbmc_10k_v3 \
-  --transcriptome=/home/liuc9/data/refdata/cellranger/refdata-gex-GRCh38-2020-A \
   --fastqs=/home/liuc9/scratch/mitochondrial/PBMC_10k_v3_10x/pbmc_10k_v3_fastqs \
   --sample=pbmc_10k_v3 \
+  --transcriptome=/mnt/isilon/xing_lab/liuc9/refdata/cellranger/refdata-gex-GRCh38-2020-A \
   --nosecondary
