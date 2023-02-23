298c2fd55480cee28554abc21c7de19e06688803
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-02-23 16:36:45 -0500

    make mgatk refs

--- utils/filter_gtf.py
+++ utils/filter_gtf.py
@@ -1,1 +1,18 @@
-spy
+#!/usr/bin/env python
+# -*- conding:utf-8 -*-
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2023-02-23 13:50:50
+# @DESCRIPTION:
+
+import os, sys
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def main():
+    run()
+
+
+if __name__ == "__main__":
+    main()
--- utils/makeref.sh
+++ utils/makeref.sh
@@ -3,7 +3,7 @@
 # cellranger mkref --config=/home/liuc9/data/refdata/mgatk_index/reference.json
 
 
-# TODO UPDATE GTF file by removing contigs that not in fasta then makeref again,
+# UPDATE GTF file by removing contigs that not in fasta then makeref again,
 
 #filter chromosome
 
@@ -12,4 +12,6 @@
 
 cellranger mkref --genome=Human \
   --fasta=/mnt/isilon/xing_lab/liuc9/refdata/mgatk_index/genome.fa \
-  --genes=/mnt/isilon/xing_lab/liuc9/refdata/ensembl/Homo_sapiens.GRCh38.107.gtf
+  --genes=/mnt/isilon/xing_lab/liuc9/refdata/mgatk_index/Homo_sapiens.GRCh38.107.new.gtf \
+  --nthreads=50 \
+  --memgb=200
