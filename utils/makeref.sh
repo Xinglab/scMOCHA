1527a107e9924bcae85424972aefaddc7f6c9d5f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-02-23 13:50:35 -0500

    filter gtf

--- utils/makeref.sh
+++ utils/makeref.sh
@@ -3,6 +3,13 @@
 # cellranger mkref --config=/home/liuc9/data/refdata/mgatk_index/reference.json
 
 
-cellranger mkref --genome=Human --fasta=/mnt/isilon/xing_lab/liuc9/refdata/mgatk_index/genome.fa --genes=/mnt/isilon/xing_lab/liuc9/refdata/ensembl/Homo_sapiens.GRCh38.107.gtf
-
 # TODO UPDATE GTF file by removing contigs that not in fasta then makeref again,
+
+#filter chromosome
+
+
+
+
+cellranger mkref --genome=Human \
+  --fasta=/mnt/isilon/xing_lab/liuc9/refdata/mgatk_index/genome.fa \
+  --genes=/mnt/isilon/xing_lab/liuc9/refdata/ensembl/Homo_sapiens.GRCh38.107.gtf
