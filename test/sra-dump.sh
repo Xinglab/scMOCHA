72da4d4b7fe31ae8b8a8c11a1c6b28f8babcc4d7
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-03-19 17:53:32 -0400

    sra dump

--- test/sra-dump.sh
+++ test/sra-dump.sh
@@ -11,9 +11,18 @@ param=$#
 sra_dir="/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc"
 
 
-fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu2/SRR11680210.sra
+fasterq-dump --mem 50G --threads 20 --split-3 --gzip ${sra_dir}/flu2/SRR11680210.sra
 
-fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu5/SRR11680214.sra
+fasterq-dump --mem 50G --threads 20 --split-3 --gzip ${sra_dir}/flu5/SRR11680214.sra
 
 
 # fasterq-dump -S --include-technical
+
+mv SRR11680210_1.fastq SRR11680210_S1_L001_R1_001.fastq
+
+mv SRR11680210_2.fastq SRR11680210_S1_L001_R2_001.fastq
+
+
+mv SRR11680214_1.fastq SRR11680214_S1_L001_R1_001.fastq
+
+mv SRR11680214_2.fastq SRR11680214_S1_L001_R2_001.fastq
