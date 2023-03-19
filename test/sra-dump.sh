0151d986c6114aeb8e9ffa586ebec30eac9ffae0
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-03-19 14:30:42 -0400

    dump

--- test/sra-dump.sh
+++ test/sra-dump.sh
@@ -14,3 +14,6 @@ sra_dir="/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc"
 fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu2/SRR11680210.sra
 
 fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu5/SRR11680214.sra
+
+
+# fasterq-dump -S --include-technical
