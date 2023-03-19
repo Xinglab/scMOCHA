38f648ca0ef93ff3ce6a86c5cf23e03f506d3c98
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-03-19 17:53:38 -0400

    cell ranger

--- test/sra-cellranger.sh
+++ test/sra-cellranger.sh
@@ -9,7 +9,23 @@ param=$#
 
 output_id="Flu2"
 fastqs="/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc/flu2"
-sample_id="Flu2"
+sample_id="SRR11680210"
+transcriptome="/home/liuc9/data/refdata/cellranger/refdata-gex-GRCh38-2020-A"
+cpu=20
+
+cellranger count \
+  --id=${output_id} \
+  --fastqs=${fastqs} \
+  --sample=${sample_id} \
+  --transcriptome=${transcriptome} \
+  --nosecondary \
+  --disable-ui \
+  --localcores ${cpu}
+
+
+output_id="Flu5"
+fastqs="/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc/flu5"
+sample_id="SRR11680214"
 transcriptome="/home/liuc9/data/refdata/cellranger/refdata-gex-GRCh38-2020-A"
 cpu=20
 
