5188785a1d7010879a30a1c505987431aa1306a2
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-03-19 13:53:22 -0400

    fasterq dump

---
+++ test/sra-dump.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2023-03-19 13:47:48
+# @DESCRIPTION:
+
+# Number of input parameters
+param=$#
+
+
+sra_dir="/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc"
+
+
+fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu2/SRR11680210.sra
+
+fasterq-dump --mem 50G --threads 20 --split-3 ${sra_dir}/flu5/SRR11680214.sra
