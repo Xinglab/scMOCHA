ec514a080e85c552ac66d70fedcc9e86f8db91f2
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-03-22 18:10:36 -0400

    scmtah and depth

---
+++ bin/mt_depth.R
@@ -0,0 +1,138 @@
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Wed Mar 22 17:19:46 2023
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(patchwork)
+library(rlang)
+library(ggtranscript)
+
+# src ---------------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+
+depthfile <- args[1]
+
+# depthfile <- "/scr1/users/liuc9/mitochondrial/testdata/1_old_donor_pbmc/flu2/Flu2/outs/MT.depth"
+
+
+# header ------------------------------------------------------------------
+
+# future::plan(future::multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+coverage <- data.table::fread(
+  input = depthfile,
+  col.names =  c("chr", "pos", "depth")
+)
+
+# body --------------------------------------------------------------------
+conn <- DBI::dbConnect(
+  duckdb::duckdb(),
+  "/mnt/isilon/xing_lab/liuc9/refdata/ensembl/Homo_sapiens.GRCh38.107.gtf.plyranges.duckdb"
+)
+
+gtf_gene <- dplyr::tbl(conn, "grch38_107_plyranges") |> 
+  dplyr::filter(seqnames == "MT") |> 
+  data.table::as.data.table()
+
+DBI::dbDisconnect(conn)
+
+
+gtf_gene |> 
+  dplyr::filter(type == "exon") |> 
+  as.data.frame() ->
+  gtf_gene_df
+
+coverage %>%
+  ggplot(aes(x=pos, y = depth)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(
+    expand = expansion(mult = c(0, 0.03)),
+    limits = c(1, 17000),
+  ) +
+  scale_y_continuous(
+    expand = c(0.01, 0)
+  ) +
+  theme(
+    plot.margin = margin(t = 0, b = 0, unit = "cm"),
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    axis.line.y.left = element_line(color = "black"),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.line.x = element_blank(),
+    axis.title.x = element_blank()
+  ) +
+  labs(y = "Depth") ->
+  p1;p1
+
+gtf_gene_df %>% 
+  ggplot(aes(
+    xstart = start,
+    xend = end,
+    y = gene_name
+  )) +
+  geom_range( aes(fill = transcript_biotype)) +
+  geom_intron(
+    data = to_intron(gtf_gene_df, "transcript_name"),
+    aes(strand = strand)
+  ) +
+  scale_x_continuous(
+    expand = expansion(mult = c(0, 0.03)),
+    limits = c(1, 17000),
+    breaks = seq(1000, 17000, 1000),
+    labels = seq(1000, 17000, 1000)
+  ) +
+  theme(
+    plot.margin = margin(t = 0, b = 0, unit = "cm"),
+    panel.background = element_blank(),
+    panel.grid = element_line(colour = "grey", linetype = "dashed"),
+    panel.grid.major = element_line(
+      colour = "grey",
+      linetype = "dashed",
+      size = 0.2
+    ),
+    axis.line = element_line(color = "black"),
+    # axis.ticks.x = element_blank(),
+    # axis.text.x = element_blank(),
+    # axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 12, color = "black"),
+    axis.title.y = element_blank(),
+    legend.position = "bottom"
+  ) +
+  labs(
+    x = "Position",
+  ) ->
+  p2;p2
+
+
+p <- cowplot::plot_grid(
+  plotlist = list(p1, p2),
+  ncol = 1,
+  align = "v",
+  rel_heights = c(0.4, 0.6)
+)
+
+ggsave(
+  filename = "MT.depth.pdf",
+  plot = p,
+  device = "pdf",
+  width = 12,
+  height = 9
+)
+
+# footer ------------------------------------------------------------------
+
+# future::plan(future::sequential)
+
+# save image --------------------------------------------------------------
---
+++ bin/scmtah.R
@@ -0,0 +1,255 @@
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Wed Mar 22 15:04:30 2023
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(patchwork)
+library(rlang)
+
+# src ---------------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+
+sc_dir <- "/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc/flu2/Flu2/outs"
+cluster_dir <- "/home/liuc9/scratch/mitochondrial/testdata/1_old_donor_pbmc/flu2/Flu2/outs" 
+
+# hetero_file <- args[1]
+# coverage_file <- args[2]
+# cluster_umap_file <- args[3]
+
+
+# header ------------------------------------------------------------------
+
+# future::plan(future::multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+hetero <- data.table::fread(input = file.path(
+  sc_dir,
+  "mgatk_out/final",
+  "sc.cell_heteroplasmic_df.tsv.gz"
+)) |> 
+  dplyr::rename(barcode = "V1") |> 
+  tidyr::pivot_longer(
+    cols = -barcode,
+    names_to = "variant",
+    values_to = "af"
+  )
+
+coverage <- data.table::fread(
+  input = file.path(
+    sc_dir,
+    "mgatk_out/final",
+    "sc.coverage.txt.gz"
+  ),
+  sep = ",",
+  col.names = c("pos", "barcode", "depth")
+) |> 
+  dplyr::mutate(depth = log2(depth + 1))
+
+coverage_wider <- coverage |> 
+  tidyr::pivot_wider(
+    names_from = pos,
+    values_from = depth
+  )
+
+cluster_umap <- data.table::fread(
+  input = file.path(
+    sc_dir,
+    "cluster_umap.tsv"
+  ),
+  sep = "\t"
+) |> 
+  dplyr::mutate(cluster = factor(cluster))
+
+# body --------------------------------------------------------------------
+
+cluster_umap %>%
+  dplyr::left_join(
+    hetero |> tidyr::pivot_wider(names_from  = variant, values_from = af), 
+    by = "barcode"
+  ) ->
+  cell_cluster_af
+
+
+cell_cluster_af %>%
+  dplyr::select(barcode, dplyr::contains(">")) %>%
+  tidyr::pivot_longer(
+    cols = -barcode,
+    names_to = "variant",
+    values_to = "af"
+  ) %>%
+  dplyr::group_by(barcode) %>%
+  dplyr::summarise(s_af = sum(af, na.rm = T)) %>%
+  dplyr::left_join(
+    cell_cluster_af %>%
+      dplyr::select(barcode, cluster) ,
+    by = "barcode"
+  ) %>%
+  dplyr::arrange(cluster, -s_af) ->
+  cell_cluster_af_col_rank
+
+cell_cluster_af %>%
+  dplyr::select(barcode, dplyr::contains(">")) %>%
+  tidyr::pivot_longer(
+    cols = -barcode,
+    names_to = "variant",
+    values_to = "af"
+  ) %>%
+  dplyr::mutate(pos = gsub(pattern = "([[:digit:]]*).*", "\\1", variant) %>% as.numeric()) ->
+  cell_cluster_af_pos
+
+
+coverage |> 
+  dplyr::filter(barcode %in% cell_cluster_af_pos$barcode) %>%
+  dplyr::filter(pos %in% cell_cluster_af_pos$pos) ->
+  coverage_pos
+
+
+cell_cluster_af_pos %>%
+  dplyr::left_join(
+    coverage_pos,
+    by = c("barcode", "pos")
+  ) %>%
+  tidyr::replace_na(
+    replace = list(
+      af = 0
+    )
+  ) %>%
+  dplyr::mutate(af = ifelse(is.na(depth), NA, af)) %>%
+  dplyr::arrange(pos) %>%
+  dplyr::select(barcode, variant, af) %>%
+  tidyr::pivot_wider(
+    names_from = "variant",
+    values_from = af
+  ) %>%
+  dplyr::slice(match(cell_cluster_af_col_rank$barcode, barcode)) %>%
+  tibble::column_to_rownames(var = "barcode") %>%
+  as.matrix() %>%
+  t() ->
+  cell_cluster_af_mtx
+
+
+library(ComplexHeatmap)
+pcc <- readr::read_tsv(file = "https://raw.githubusercontent.com/chunjie-sam-liu/chunjie-sam-liu.life/master/public/data/pcc.tsv")
+
+
+
+cell_cluster_af %>%
+  dplyr::select(barcode, cluster) %>%
+  dplyr::slice(match(colnames(cell_cluster_af_mtx), barcode)) %>%
+  tibble::column_to_rownames(var = "barcode") %>%
+  dplyr::select(Cluster = cluster) ->
+  cell_cluster_af_cluster
+
+col_clusters <- levels(cell_cluster_af_cluster$Cluster) %>% as.numeric()
+col_colors <- pcc$color[1:length(levels(cell_cluster_af_cluster$Cluster))]
+
+names(col_colors) <- col_clusters
+
+chm_top <- ComplexHeatmap::HeatmapAnnotation(
+  df = cell_cluster_af_cluster,
+  gap = unit(c(2,2), "mm"),
+  col = list(Cluster = col_colors),
+  which = "column"
+)
+
+
+ComplexHeatmap::Heatmap(
+  matrix = cell_cluster_af_mtx,
+  col = circlize::colorRamp2(
+    breaks = c(0, 1),
+    colors = c("white", "red"),
+    space = "RGB"
+  ),
+  name = "Allele Freq",
+  na_col = "grey",
+  color_space = "LAB",
+  rect_gp = gpar(col = NA),
+  border = NA,
+  cell_fun = NULL,
+  layer_fun = NULL,
+  jitter = FALSE,
+  # row
+  cluster_rows = F,
+  cluster_row_slices = T,
+  clustering_distance_rows = "pearson",
+  clustering_method_rows = "ward.D",
+  # column
+  cluster_columns = FALSE,
+  cluster_column_slices = T,
+  # clustering_distance_columns = "pearson",
+  # clustering_method_columns = "ward.D",
+  show_column_names = FALSE,
+  
+  top_annotation = chm_top
+) ->
+  chm;chm
+
+
+
+cell_cluster_af_pos %>%
+  dplyr::left_join(
+    coverage_log2_pos,
+    by = c("barcode", "pos")
+  ) %>%
+  dplyr::select(barcode, pos, depth) %>%
+  dplyr::arrange(pos) %>%
+  tidyr::pivot_wider(
+    names_from = pos,
+    values_from = depth
+  ) %>%
+  dplyr::slice(match(cell_cluster_af_col_rank$barcode, barcode)) %>%
+  tibble::column_to_rownames(var = "barcode") %>%
+  as.matrix() %>%
+  t() ->
+  depth_mtx
+
+ComplexHeatmap::Heatmap(
+  matrix = depth_mtx,
+  col = circlize::colorRamp2(
+    breaks = c(0, 4),
+    colors = c("white", "red"),
+    space = "RGB"
+  ),
+  name = "log2(Depth+1)",
+  na_col = "grey",
+  color_space = "LAB",
+  rect_gp = gpar(col = NA),
+  border = NA,
+  cell_fun = NULL,
+  layer_fun = NULL,
+  jitter = FALSE,
+  # row
+  cluster_rows = F,
+  cluster_row_slices = T,
+  clustering_distance_rows = "pearson",
+  clustering_method_rows = "ward.D",
+  # column
+  cluster_columns = FALSE,
+  cluster_column_slices = T,
+  # clustering_distance_columns = "pearson",
+  # clustering_method_columns = "ward.D",
+  show_column_names = FALSE,
+  
+  top_annotation = chm_top
+) ->
+  chm_depth;chm_depth
+
+
+
+
+# footer ------------------------------------------------------------------
+
+future::plan(future::sequential)
+
+# save image --------------------------------------------------------------
