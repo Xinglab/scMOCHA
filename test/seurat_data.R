02b577f7afbcfaaaf7ef30b577f5db018f639514
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-04-06 15:23:02 -0400

    seurat_data

---
+++ test/seurat_data.R
@@ -0,0 +1,49 @@
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Thu Apr  6 15:17:39 2023
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(patchwork)
+library(rlang)
+library(Seurat)
+library(SeuratData)
+
+
+# args --------------------------------------------------------------------
+
+
+# src ---------------------------------------------------------------------
+
+
+# header ------------------------------------------------------------------
+
+# future::plan(future::multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+
+# body --------------------------------------------------------------------
+
+# SeuratData::InstalledData()
+SeuratData::AvailableData() |> 
+  dplyr::filter(!Installed) |> 
+  dplyr::pull(Dataset) |> 
+  purrr::map(
+    .f = SeuratData::InstallData
+  )
+
+
+# footer ------------------------------------------------------------------
+
+# future::plan(future::sequential)
+
+# save image --------------------------------------------------------------
