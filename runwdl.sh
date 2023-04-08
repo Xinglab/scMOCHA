c0ac232beeea045b7d4987eacefaabd3255abd25
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-04-08 00:25:00 -0400

    runwdl

--- runwdl.sh
+++ runwdl.sh
@@ -7,10 +7,11 @@
 # Number of input parameters
 # param=$#
 # cd /home/liuc9/tmp/mito/flu2
-# nohup java -Dconfig.file=/home/liuc9/github/scRNAseq-MitoVariant/config/ref.conf \
-#     -jar /home/liuc9/tools/cromwell-78.jar \
-#     run /home/liuc9/github/scRNAseq-MitoVariant/cellranger.wdl \
-#     -i /home/liuc9/github/scRNAseq-MitoVariant/cellranger.json &
+module load Java/15.0.1
+nohup java -Dconfig.file=/home/liuc9/github/scRNAseq-MitoVariant/config/ref.conf \
+    -jar /home/liuc9/tools/cromwell-78.jar \
+    run /home/liuc9/github/scRNAseq-MitoVariant/cellranger.wdl \
+    -i /home/liuc9/github/scRNAseq-MitoVariant/cellranger-a.json &
 
 java -jar /home/liuc9/tools/cromwell-78.jar \
     run /home/liuc9/github/scRNAseq-MitoVariant/cellranger.wdl \
