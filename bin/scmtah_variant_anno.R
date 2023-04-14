3660e475340020926e4a8527f65729bc0de38506
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-04-14 17:08:53 -0400

    variant anno

--- bin/scmtah_variant_anno.R
+++ bin/scmtah_variant_anno.R
@@ -14,7 +14,7 @@ library(rlang)
 library(httr)
 
 # args --------------------------------------------------------------------
-
+# load(file = "/scr1/users/liuc9/tmp/mito/flu2-a/cromwell-executions/SCMTAH/0138fcd0-c384-42c2-8704-6647767610d2/call-plot_scmtah/execution/scmtah.rda")
 
 # src ---------------------------------------------------------------------
 
@@ -53,7 +53,7 @@ cell_raw_cluster_forplot$forplot |>
   ) ->
   cell_variants
 
-
+# 
 readr::write_tsv(
   x = cell_variants,
   file = "cell_snvlist.tsv"
@@ -86,7 +86,9 @@ tryCatch(
 
 
 cluster_cluster_forplot$forplot |> 
-  dplyr::filter(depth > 3) |> 
+  dplyr::filter(depth > 3) |>  
+  dplyr::select(pos, variant) |>  
+  dplyr::distinct() |> 
   dplyr::mutate(variant = gsub(
     pattern = "[0-9]*",
     replacement = "",
@@ -96,19 +98,54 @@ cluster_cluster_forplot$forplot |>
     col = variant,
     into = c("ref", "var")
   ) |> 
+  dplyr::mutate(sample = "Sample1") |> 
   dplyr::select(
-    sample = barcode, 
+    sample = sample, 
     pos = pos, 
     ref = ref,
     var = var
   ) ->
   cluster_variants
 
+readr::write_tsv(
+  x = cluster_variants,
+  file = "cluster_snvlist.tsv"
+)
+tryCatch(
+  {
+    response <- POST(
+      "https://mitomap.org/mitomaster/websrvc.cgi",
+      body = list(
+        file = upload_file("cluster_snvlist.tsv"),
+        fileType = "snvlist",
+        output = "detail"
+      ),
+      encode = "multipart"
+    )
+    # print(content(response, "text"))
+  },
+  error = function(err) {
+    print(paste("HTTP error:", err$message))
+  },
+  warning = function(w) {
+    print(paste("Warning:", w$message))
+  },
+  finally = {
+    print("Done.")
+  }
+)
 
-
+a <- content(
+  x = response,
+  as = "text",
+  encoding = "UTF-8"
+) |> 
+  data.table::fread(
+    sep = "\t"
+  )
 
 # footer ------------------------------------------------------------------
 
-future::plan(future::sequential)
+# future::plan(future::sequential)
 
 # save image --------------------------------------------------------------
