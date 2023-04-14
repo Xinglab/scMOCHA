dd81bb88c7ff146bf9eda88567ee6a0923b9ab87
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-04-13 22:45:06 -0400

    variant annotation

---
+++ test/variant-annotation.R
@@ -0,0 +1,7 @@
+
+
+# variant annotations from MITOMAP
+# http://www.mitomap.org/variants.php
+# http://www.mitomap.org/download.php
+# http://www.mitomap.org/variants.php?chr=MT&pos=16069&ref=A&alt=G
+# http://www.mitomap.org/variants.php?chr=MT&pos=16069&ref=A&alt=T
