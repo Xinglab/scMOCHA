7d4d01cae40ae33ccfc56aac3fa681c66dfb4d24
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-04-27 17:22:35 -0400

    update

--- runs/01-Sci_Immunol_32651212/01-generate-dump-scripts.R
+++ runs/01-Sci_Immunol_32651212/01-generate-dump-scripts.R
@@ -52,62 +52,7 @@ sratable |>
   srafiles
 
 
-
 srafiles |> 
-  dplyr::mutate(
-    dump_slrm = purrr::map2_chr(
-      .x = srrdir,
-      .y = srafile,
-      .f = function(.x, .y) {
-        .srrid <- basename(.x)
-        
-        cmd_slrm <- c(
-          "#!/usr/bin/env bash",
-          "# @AUTHOR: Chun-Jie Liu",
-          "# @CONTACT: chunjie.sam.liu.at.gmail.com",
-          "# @DATE: {lubridate::now()}" |> glue::glue(),
-          "",
-          "#SBATCH --signal=USR2",
-          "#SBATCH --ntasks=1",
-          "#SBATCH --cpus-per-task=10",
-          "#SBATCH --mem=50G",
-          "#SBATCH --time=720:00:00",
-          "#SBATCH --output={.x}/dump.job.{.srrid}.%j" |> glue::glue(),
-          "module load R/4.1.0"
-        )
-        cmd_dump <- c(
-          "/home/liuc9/tools/sratoolkit.2.11.2-centos_linux64/bin/fasterq-dump {.y} --include-technical --mem 50G --threads 10 --split-files --outdir {.x}" |> glue::glue()
-        )
-        cmd_rename <- c(
-          "mv {.x}/{.srrid}_1.fastq {.x}/{.srrid}_S1_L001_R1_001.fastq" |> glue::glue(),
-          "mv {.x}/{.srrid}_2.fastq {.x}/{.srrid}_S1_L001_R2_001.fastq" |> glue::glue()
-        )
-        
-        cmd <- c(
-          cmd_slrm,
-          "",
-          cmd_dump,
-          "",
-          cmd_rename
-        )
-        
-        dump_slrm_file <-  file.path(
-          .x,
-          "dump_{.srrid}.slrm" |> glue::glue()
-        )
-        
-        readr::write_lines(
-          cmd,
-          file = dump_slrm_file
-        )
-        dump_slrm_file
-      }
-    )
-  ) ->
-  sarfiles_dump
-
-
-sarfiles_dump |> 
   dplyr::mutate(
     conf_json = purrr::map_chr(
       .x = srrdir,
@@ -138,10 +83,11 @@ sarfiles_dump |>
       }
     )
   ) ->
-  sarfiles_dump_conf
+  srafiles_conf
 
 
-sarfiles_dump_conf |> 
+
+srafiles_conf |> 
   dplyr::mutate(
     scmtah_sh = purrr::map_chr(
       .x = srrdir,
@@ -183,7 +129,70 @@ sarfiles_dump_conf |>
       }
     )
   ) ->
+  srafiles_conf_scmtah
+
+srafiles_conf_scmtah |> 
+  dplyr::mutate(
+    dump_slrm = purrr::map2_chr(
+      .x = srrdir,
+      .y = srafile,
+      .f = function(.x, .y) {
+        .srrid <- basename(.x)
+        
+        cmd_slrm <- c(
+          "#!/usr/bin/env bash",
+          "# @AUTHOR: Chun-Jie Liu",
+          "# @CONTACT: chunjie.sam.liu.at.gmail.com",
+          "# @DATE: {lubridate::now()}" |> glue::glue(),
+          "",
+          "#SBATCH --signal=USR2",
+          "#SBATCH --ntasks=1",
+          "#SBATCH --cpus-per-task=10",
+          "#SBATCH --mem=50G",
+          "#SBATCH --time=720:00:00",
+          "#SBATCH --output={.x}/dump.job.{.srrid}.%j" |> glue::glue(),
+          "#module load R/4.1.0"
+        )
+        cmd_dump <- c(
+          "/home/liuc9/tools/sratoolkit.2.11.2-centos_linux64/bin/fasterq-dump {.y} --temp /scr1/users/liuc9/tmp/fasterq_dump  --include-technical --mem 50G --threads 10 --split-files --outdir {.x}" |> glue::glue()
+        )
+        cmd_rename <- c(
+          "mv {.x}/{.srrid}_1.fastq {.x}/{.srrid}_S1_L001_R1_001.fastq" |> glue::glue(),
+          "mv {.x}/{.srrid}_2.fastq {.x}/{.srrid}_S1_L001_R2_001.fastq" |> glue::glue()
+        )
+        
+        cmd_runwdl <- c(
+          "bash {.x}/runwdl_{.srrid}.sh" |> glue::glue()
+        )
+        cmd <- c(
+          cmd_slrm,
+          "",
+          cmd_dump,
+          "",
+          cmd_rename,
+          "",
+          cmd_runwdl
+        )
+        
+        
+        dump_slrm_file <-  file.path(
+          .x,
+          "dump_{.srrid}.slrm" |> glue::glue()
+        )
+        
+        readr::write_lines(
+          cmd,
+          file = dump_slrm_file
+        )
+        dump_slrm_file
+      }
+    )
+  ) ->
   sarfiles_dump_conf_scmtah
+
+
+
+
 
 
 readr::write_tsv(
