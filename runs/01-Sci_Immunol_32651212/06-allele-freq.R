d5295f8595776cd5cfc925565295e813e36dc16f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-04 17:31:30 -0400

    upset

--- runs/01-Sci_Immunol_32651212/06-allele-freq.R
+++ runs/01-Sci_Immunol_32651212/06-allele-freq.R
@@ -47,7 +47,7 @@ metadata_anno_depth |>
           dplyr::mutate(
             variant = glue::glue("{tpos}{tnt}>{qnt}")
           ) |> 
-          dplyr::pull(variant)
+          dplyr::select(variant)
       }
     )
   ) ->
@@ -55,20 +55,123 @@ metadata_anno_depth |>
 
 
 metadata_anno_depth_variant |> 
-  dplyr::select(srrid, source_name, variant) |> 
+  dplyr::mutate(color = dplyr::case_match(
+    source_name,
+    "Flu_PBMC" ~ggsci::pal_jama()(4)[[1]],
+    "Normal_PBMC" ~ ggsci::pal_jama()(4)[[2]],
+    "nCoV_PBMC(mild)" ~ ggsci::pal_jama()(4)[[3]],
+    "nCoV_PBMC(severe)" ~ ggsci::pal_jama()(4)[[4]]
+  )) |> 
+  dplyr::select(srrid, source_name, variant, color) |> 
   dplyr::filter(!purrr::map_lgl(.x = variant, .f = is.null)) ->
   for_variant
 
 
 for_variant |> 
-  dplyr::filter(source_name == "Normal_PBMC") |> 
   dplyr::select(srrid, variant) |> 
-  tibble::deframe() ->
-  d
+  tibble::deframe() |> 
+  purrr::reduce(.f = union) -> all_variants
 
-ggvenn::ggvenn(
-  data = d
-)
+for_variant |> 
+  dplyr::select(srrid, variant) |> 
+  tibble::deframe() |> 
+  purrr::reduce(.f = intersect) -> common_variants
+
+
+fn_upset_plot <- function(.x) {
+  # .x <- "nCoV_PBMC(severe)"
+  library(ggupset)
+  for_variant |> 
+    dplyr::filter(source_name == .x) ->
+    d
+  
+  d |> 
+    tidyr::unnest(cols = variant) |> 
+    dplyr::select(-source_name) |> 
+    dplyr::group_by(variant) |> 
+    tidyr::nest() |> 
+    dplyr::ungroup() |> 
+    dplyr::mutate(
+      srrid = purrr::map(
+        .x = data,
+        .f = function(.x) {
+          .x |> dplyr::pull(srrid)
+        }
+      ) 
+    ) |> 
+    dplyr::select(-data) |> 
+    ggplot(aes(x = srrid)) +
+    geom_bar(width = 0.6, fill = d$color[1]) +
+    geom_text(
+      stat='count', 
+      aes(label=after_stat(count)), 
+      vjust = -0.5,
+      color = "black",
+      size = 6,
+      fontface = "bold"
+    ) +
+    scale_x_upset(order_by = "degree") +
+    scale_y_continuous(
+      expand = expansion(mult = c(0, 0.1), add = 0)
+    ) +
+    theme_combmatrix(
+      combmatrix.label.make_space = TRUE,
+      combmatrix.panel.point.color.fill = d$color[1],
+      combmatrix.panel.line.size = 0,
+      combmatrix.label.text = element_text(
+        size = 12, 
+        color = "black", 
+        face = "bold"
+      ),
+      combmatrix.label.extra_spacing = 5,
+      combmatrix.panel.striped_background.color.one = "white",
+      combmatrix.panel.striped_background.color.two = "grey",
+    ) +
+    labs(
+      y = "# of Variants",
+      x = "",
+      title = .x
+    ) +
+    theme(
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      axis.line = element_line(size = 0.5, color = "black"),
+      axis.title.y = element_text(
+        size = 16,
+        color = "black",
+        face = "bold"
+      ),
+      axis.text.y = element_text(
+        size = 14,
+        color = "black"
+      ),
+      plot.title = element_text(
+        hjust = 0.5, 
+        color = "black",
+        size = 16, 
+        face = "bold"
+      )
+    ) ->
+    .p_up
+  
+  ggsave(
+    plot = .p_up,
+    filename = "upset-{.x}.pdf" |> glue::glue(),
+    path = "/home/liuc9/github/scRNAseq-MitoVariant/01-Sci_Immunol_32651212/outputs",
+    width = 8, 
+    height = 6,
+    device = "pdf"
+  )
+}
+
+metadata_anno_depth_variant$source_name |> 
+  unique() |> 
+  purrr::map(
+    .f = fn_upset_plot
+  )
+
+
+
 # footer ------------------------------------------------------------------
 
 future::plan(future::sequential)
