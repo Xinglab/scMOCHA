94d6b9b6070b12c9af81e40d462bac4af99ce64a
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-08-10 16:48:32 -0400

    azimuth data

--- runs/03-ADKP/04-azimuth-data.R
+++ runs/03-ADKP/04-azimuth-data.R
@@ -77,6 +77,39 @@ SeuratData::AvailableData() |>
 }
 
 
+azd_celltype |> 
+  dplyr::mutate(
+    b = furrr::future_map_chr(
+      .x = a,
+      .f = \(.a) {
+        as.list(.a) |> 
+          purrr::map(.f = levels) |> 
+          purrr::map(.f = length) |> 
+          tibble::enframe() |> 
+          tidyr::unnest(cols = value) |> 
+          dplyr::mutate(nv = glue::glue("{name} (n={value})")) ->
+          .aa
+        
+        paste0(.aa$nv, collapse = "; ")
+      }
+    )
+  ) ->
+  azd_celltype_n
+
+
+azd_celltype_n |> 
+  dplyr::select(-a) |> 
+  dplyr::rename(
+    Tissue = system,
+    Species = species,
+    Ncells = ncells,
+    Tech = tech,
+    Celltypes = b
+  ) |> 
+  writexl::write_xlsx(
+    path = "/home/liuc9/github/scMOCHA/03-ADKP/azimuth_celltype.xlsx"
+  )
+
 
 # footer ------------------------------------------------------------------
 
