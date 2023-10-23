38cc919122fea3f7ca13ed603d4c6a84f5cd96a8
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-10-23 16:31:09 -0400

    make run

---
+++ runs/05-Liming/02-run-step-by-step.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Mon Oct 23 16:26:12 2023
+# @DESCRIPTION: filename
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(patchwork)
+library(prismatic)
+library(data.table)
+#library(rlang)
+
+# args --------------------------------------------------------------------
+
+
+# src ---------------------------------------------------------------------
+
+
+# header ------------------------------------------------------------------
+
+# future::plan(future::multisession, workers = 10)
+
+# function ----------------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+runfiles <- data.table::fread(
+  input =  file.path(
+    "/home/liuc9/github/scMOCHA/runs/05-Liming",
+    "runfiles.csv"
+  ),
+  sep = ","
+)
+
+
+# body --------------------------------------------------------------------
+
+runfiles$scmocha_sh |> 
+  purrr::map_chr(
+    .f = \(.x) {
+      "bash {.x} &" |> glue::glue()
+    }
+  ) |> 
+  readr::write_lines(
+    file = file.path(
+      "/home/liuc9/github/scMOCHA/05-Liming/scmocha-celline",
+      "runwdl.sh"
+    )
+  )
+
+# footer ------------------------------------------------------------------
+
+# future::plan(future::sequential)
+
+# save image --------------------------------------------------------------
