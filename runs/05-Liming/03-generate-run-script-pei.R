afd132c8dcc472c8b361e5dc3eebbb9daee3597f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-10-26 17:55:03 -0400

    generage local scripts

--- runs/05-Liming/03-generate-run-script-pei.R
+++ runs/05-Liming/03-generate-run-script-pei.R
@@ -121,9 +121,9 @@ metadata |>
           "# @DATE: {lubridate::now()}" |> glue::glue(),
           "",
           "module load Java/15.0.1",
-          "nohup java -Dconfig.file=/home/liuc9/github/scMOCHA/config/singularity.slurm.conf \\",
+          "nohup java -Dconfig.file=/home/liuc9/github/scMOCHA/config/slurm.conf \\",
           "-jar /home/liuc9/tools/cromwell-78.jar \\",
-          "run /home/liuc9/github/scMOCHA/scMOCHA-image.wdl \\",
+          "run /home/liuc9/github/scMOCHA/scMOCHA.wdl \\",
           "-i {.jsonfile} 1>{.logfile} 2>{.errfile} &" |> glue::glue()
         )
         
